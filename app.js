1e773850b93378a215f24fb5ffd3ffbe3df185d5
Piyabuthong <piyabut2806@gmail.com>
2025-09-28 22:22:06 +0700

    add app.js del2-6

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-console.log("Hello, World!"); // This is a simple Node.js application that prints "Hello, World!" to the console.
-console.log("This is a simple Node.js application that prints 'Hello, World!' to the console.");  // Added for demonstration purposes.
-console.log("This line is added to show recent edits in the file."); // Another line added for demonstration.
-console.log("line4."); // Another line added for demonstration.
-console.log("line5."); // Another line added for demonstration.
-console.log("line6.");
+console.log("Hello, sun"); // This is a simple Node.js application that prints "Hello, sun" to the console.
+
